c5d916c58cb7dd8dd26170a028cfd1191f976abb
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-02-25 17:45:18 -0800

    issues with eslint

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import  ROUTES  from '../../routes';
+import ROUTES from '../../routes';
 
 export default function App() {
   return (
